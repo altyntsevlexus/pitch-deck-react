b8dfd18ffdc80af39b309f9f3ce88aca0bd2aa65
altyntsevlexus <point-alex10@ukr.net>
2021-10-06 17:45:30 +0300

    Keep calm and commit

--- src/route/AppRouter.jsx
+++ src/route/AppRouter.jsx
@@ -30,7 +30,6 @@ const AppRouter = () => {
       <Switch>
         {ROUTE_CONFIG.map((route, i) => {
           return (
-            // eslint-disable-next-line react/no-array-index-key
             <Route key={i} path={route.path}>
               <route.component theme={route.theme} />
             </Route>
