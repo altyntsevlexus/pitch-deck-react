add0bbb544bfb8605c395ba5ffdedc8bee82218e
altyntsevlexus <point-alex10@ukr.net>
2021-10-07 13:09:31 +0300

    react/no-array-index-key in Router

--- src/route/AppRouter.jsx
+++ src/route/AppRouter.jsx
@@ -30,6 +30,7 @@ const AppRouter = () => {
       <Switch>
         {ROUTE_CONFIG.map((route, i) => {
           return (
+            // eslint-disable-next-line react/no-array-index-key
             <Route key={i} path={route.path}>
               <route.component theme={route.theme} />
             </Route>
